9de827fbc89f57c12cdc515e1c349961572e9fb2
Eduardo Schettino <schettino72@gmail.com>
2018-07-02 15:37:02 +0800

    Include implicit deps from file_dep/target relations.

--- doit_graph.py
+++ doit_graph.py
@@ -1,5 +1,10 @@
 import pygraphviz
+
 from doit.cmd_base import DoitCmdBase
+from doit.control import TaskControl
+
+
+
 
 class GraphCmd(DoitCmdBase):
     doc_purpose = "create task's dependency-graph image"
@@ -7,12 +12,12 @@ class GraphCmd(DoitCmdBase):
     cmd_options = tuple()
 
     def _execute(self):
-        tasks = dict([(t.name, t) for t in self.task_list])
+        control = TaskControl(self.task_list)
 
         graph = pygraphviz.AGraph(strict=False, directed=True)
         graph.node_attr['color'] = 'lightblue2'
         graph.node_attr['style'] = 'filled'
-        for task in tasks.values():
+        for task in control.tasks.values():
             graph.add_node(task.name)
             for sink in task.task_dep:
                 graph.add_edge(task.name, sink)
